4028248e4ed0db9b042d36cebbb9783223e89754
Ragazzoforte <michel.vollmuller@gmail.com>
2024-04-24 13:20:58 +0200

    .gitignore

---
+++ Doxygen Output/html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "2drs",
+  1: "r",
+  2: "2ds"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files",
+  2: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Files",
+  2: "Pages"
+};
+

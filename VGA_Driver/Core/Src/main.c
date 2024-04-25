8a220ac5b9891c16c0b0b58236174e5d97433f0d
TimWannet <tim.wannet@student.hu.nl>
2024-04-25 14:11:07 +0200

    Removed Cmake.txt | This is needed for everyone |

--- VGA_Driver/Core/Src/main.c
+++ VGA_Driver/Core/Src/main.c
@@ -24,7 +24,7 @@
 /* USER CODE BEGIN Includes */
 
 /* USER CODE END Includes */
-
+//tes
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
 

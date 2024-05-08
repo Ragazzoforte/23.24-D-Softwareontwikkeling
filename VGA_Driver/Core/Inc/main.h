1c3c6ac0c32195908e355212c3e42d71e49067e1
Ragazzoforte <michel.vollmuller@gmail.com>
2024-05-08 13:32:11 +0200

    Arial and consolas front in normal, bold and italic in 2 sizes

--- VGA_Driver/Core/Inc/main.h
+++ VGA_Driver/Core/Inc/main.h
@@ -32,6 +32,8 @@ extern "C" {
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
+#include <stdint.h>
+
 /* USER CODE END Includes */
 
 /* Exported types ------------------------------------------------------------*/

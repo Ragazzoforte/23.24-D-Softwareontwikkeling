5e4b69449cab40ff3741f2484a78dc9b0e41c213
tijmcraft <Tijmcraft@gmail.com>
2024-05-08 17:34:55 +0200

    uart header en source files zijn toegevoegd

---
+++ VGA_Driver/Core/Inc/uart.h
@@ -0,0 +1,9 @@
+#ifndef UART_H
+#define UART_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+
+
+#endif // UART_H
---
+++ VGA_Driver/Core/Src/uart.c
@@ -0,0 +1,31 @@
+/**
+  ******************************************************************************
+  * @file    uart.c
+  * @author  Tijmen Willems
+  * @brief   uart driver functions source file
+  *
+  *   This file provides five functions to be called from the logic_layer:
+  *      - uart_init(): used to initialize the uart communication 
+  *
+  *      - API_draw_line(): used to draw a line to the VGA screen.  
+  *                                     
+  *      - API_draw_rectangle(): used to draw a rectangle to the VGA screen.
+  * 
+  *      - API_draw_bitmap(): used to draw a figure (bitmap) to the VGA screen.  
+  * 
+  *      - API_clearscreen(): used to clear the VGA screen.  
+  * 
+  ****************************************************************************** 
+  */
+
+/** @addtogroup VGA driver functions
+  * @{
+  */
+
+#include "uart.h"
+
+
+
+/**
+  * @}
+  */

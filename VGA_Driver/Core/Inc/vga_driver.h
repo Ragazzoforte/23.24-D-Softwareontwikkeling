2de587ec8a723c1a011e1538cf4d3bd34e7606a8
TimWannet <timwannet@tutamail.com>
2024-05-11 21:13:40 +0200

    first definition for the functions: api_clearscreen() and Hash()

--- VGA_Driver/Core/Inc/vga_driver.h
+++ VGA_Driver/Core/Inc/vga_driver.h
@@ -5,6 +5,7 @@ int API_draw_text (int x_lup, int y_lup, int color, char *text, char *fontname,i
 int API_draw_line (int x_1, int y_1, int x_2, int y2, int color, int weight, int reserved);
 int API_draw_rectangle (int x, int y, int width, int height, int color, int filled, int reserved1, int reserved2); // e.g.: weight, bordercolor
 int API_draw_bitmap (int x_lup, int y_lup, int bm_nr);
-int API_clearscreen (int color);
+int API_clearscreen (char *color);
+unsigned long hash(char *str);
 
 #endif /* vga_driver_h */

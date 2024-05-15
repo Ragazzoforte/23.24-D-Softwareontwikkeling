17b138b1746133ca156da39ca9205b773cd96f9e
Tim Wannet <99477919+TimWannet@users.noreply.github.com>
2024-05-15 09:48:03 +0200

    Feature/clearscreen (#35)
    * Template switch case
    
    * Change includes to be all in main.h
    
    * New function: Convert string to unique code with a hash | #defines for all colours | Tested: ok |
    
    * #define for all colours with a hex code, to be used inside the UB_VGA_FillScreen function
    
    * first definition for the functions: api_clearscreen() and Hash()
    
    * all colours included in the switch case with corresponding functions
    
    * update hex codes for coresponding colours | All colours are tested |
    
    * Remove the test function

--- VGA_Driver/Core/Src/vga_driver.c
+++ VGA_Driver/Core/Src/vga_driver.c
@@ -21,9 +21,32 @@
 /** @addtogroup VGA driver functions
   * @{
   */
-
+//--------------------------------------------------------------
+// Includes
+//--------------------------------------------------------------
 #include "main.h"
-#include "vga_driver.h"
+
+//--------------------------------------------------------------
+
+//--------------------------------------------------------------
+// Defines
+//--------------------------------------------------------------
+#define ZWART         540422306
+#define LICHTMAGENTA  338820699
+#define MAGENTA       3940791655
+#define BLAUW         511564997
+#define LICHTBLAUW    1778211001
+#define CYAAN         513217430
+#define LICHTCYAAN    1779863434
+#define GROEN         517724933
+#define LICHTGROEN    1784370937
+#define GEEL          15674151
+#define ROOD          16080670
+#define LICHTROOD     4089130130
+#define BRUIN         511801994
+#define GRIJS         517718569
+#define WIT           492542
+//--------------------------------------------------------------
 
 /**
   * @brief  API_draw_text() is used to draw a string to the VGA screen.  
@@ -80,16 +103,85 @@ int API_draw_bitmap (int x_lup, int y_lup, int bm_nr)
 /**
   * @brief  API_clearscreen() is used to clear the VGA screen.  
   *           
-  * @note   selected color must be predefined         
+  * @note   selected color must be predefined, valid colours: zwart, lichtmagenta, magenta, blauw, lichtblauw, cyaan, lichtcyaan groen, lichtgroen, geel, rood, lichtrood, bruin, grijs, wit    
   *     
-  * @param  
-  * @retval 
+  * @param colour: the color to clear the screen with
+  * @retval none
   */
-int API_clearscreen (int color)
+int API_clearscreen (char *colour)
 {
+  unsigned long colour_choice = hash(colour); //convert the string to a unique value for the switch case
 
+  switch (colour_choice) 
+  {
+    case ZWART:
+    UB_VGA_FillScreen(VGA_COL_BLACK); //VGA display is cleared with the colour: zwart
+      break;
+    case LICHTMAGENTA:
+      UB_VGA_FillScreen(VGA_COL_LIGHT_MAGENTA); //VGA display is cleared with the colour: lichtmagenta
+      break;
+    case MAGENTA:
+      UB_VGA_FillScreen(VGA_COL_MAGENTA); //VGA display is cleared with the colour: magenta
+      break;
+    case BLAUW:
+      UB_VGA_FillScreen(VGA_COL_BLUE); //VGA display is cleared with the colour: blauw
+      break;
+    case LICHTBLAUW:
+      UB_VGA_FillScreen(VGA_COL_LIGHT_BLUE); //VGA display is cleared with the colour: lichtblauw
+      break;
+    case CYAAN:
+      UB_VGA_FillScreen(VGA_COL_CYAN); //VGA display is cleared with the colour: cyaan
+      break;
+    case LICHTCYAAN:
+      UB_VGA_FillScreen(VGA_COL_LIGHT_CYAN); //VGA display is cleared with the colour: lichtcyaan
+      break;
+    case GROEN:
+      UB_VGA_FillScreen(VGA_COL_GREEN); //VGA display is cleared with the colour: groen
+      break;
+    case LICHTGROEN:
+      UB_VGA_FillScreen(VGA_COL_LIGHT_GREEN); //VGA display is cleared with the colour: lichtgroen
+      break;
+    case GEEL:
+      UB_VGA_FillScreen(VGA_COL_YELLOW); //VGA display is cleared with the colour: geel
+      break;
+    case ROOD:
+      UB_VGA_FillScreen(VGA_COL_RED); //VGA display is cleared with the colour: rood
+      break;
+    case LICHTROOD:
+      UB_VGA_FillScreen(VGA_COL_LIGHT_RED); //VGA display is cleared with the colour: lichtrood
+      break;
+    case BRUIN:
+      UB_VGA_FillScreen(VGA_COL_BROWN); //VGA display is cleared with the colour: bruin
+      break;
+    case GRIJS:
+      UB_VGA_FillScreen(VGA_COL_GREY); //VGA display is cleared with the colour: grijs
+      break;
+    case WIT:
+      UB_VGA_FillScreen(VGA_COL_WHITE); //VGA display is cleared with the colour: wit
+      break;
+    default:
+      // Code to handle invalid color input
+      break;
+  }
+return 0;
 }
 
+/**
+  * @brief hash() is used to hash a string to a unique value. 
+  *           
+  * @note  This function is used to generate a unique value for the string.   
+  *     
+  * @param str: the string to be hashed
+  * @retval unsigned long: the hashed value of the string
+  */
+unsigned long hash(char *str) {
+    unsigned long hash = 10; //seed value for spreading out the hash values
+    int c;
+
+    while ((c = *str++)) //iterate over the string
+        hash = ((hash << 5) + hash) + c;
+    return hash;
+}
 /**
   * @}
   */
